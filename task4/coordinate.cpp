c42d4df61969f9fee70464b8311faf0d04c7c535
topperal <72097569+topperal@users.noreply.github.com>
2022-11-23 16:14:19 +0300

    Update coordinate.cpp

--- task4/coordinate.cpp
+++ task4/coordinate.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
-//МЕТОД НАИСКОРЕЙШЕГО ПОКООРДИНАТНОГО СПУСКА (МНПС)
-double eps = pow(10, -6); //погрешность
+//МЕТОД НАИСКОРЕЙШЕГО ГРАДИЕНТНОГО СПУСКА (МНГС)
+double eps = pow(10, -6);//погрешность
 int countX = 0;
-const unsigned int n = 3; //размерность матрицы
+const unsigned int n = 3;//размерность матрицы
 
+
+//f(x, y, z) = 2x^2+ (3 + 0, 1·N)y^2+ (4 + 0, 1·N)z^2+ xy−yz + xz + x−2y + 3z + N, N = 13
 double mA[n][n] = {
     { 4, 1, 1},
     { 1, 8.6, -1},
@@ -13,123 +15,95 @@ double mA[n][n] = {
 
 double mB[n] = { 1, -2, 3 };
 
-double currX[n] = { 0, 0, 0 };//вектор текущего решения
+double currX[n] = { 1, 0, 0 };//вектор текущего решения
 
 double calcX[n] = { 0, 0, 0 };//вектор вычисленного решения
 
 double mQ[n] = { 0, 0, 0 };
 
-double calcF[n] = { 0, 0, 0 };//вектор вычисленных значений функций по ортам (нужно, чтобы потом выбрать тот орт, в направлении которого происходит
-                              //наибольшее убывание функции)
-
-double currF = 13;//значение функции в точке (0, 0, 0)
-
 double m = 0;
 
-//вычисляем числитель
-double calculateNumerator(double mA[n][n],double currX[n], double mB[n], double mE[n]) {
-    //вычисляем q = A*current_X+b, где A, current_X и b - матрицы
+void func(double mA[n][n], double mB[n], double currX[n]) {
+    double f = 0.5 * (mA[0][0] * currX[0] * currX[0]  + mA[1][1] * currX[1] * currX[1] + mA[2][2] * currX[2] * currX[2] + 
+        (mA[0][1] + mA[1][0])* currX[0] * currX[1] + (mA[0][2] + mA[2][0]) * currX[0] * currX[2] + (-mA[1][2] - mA[2][1]) * currX[1] * currX[2]) 
+        + currX[0] * mB[0] - currX[1] * mB[1] + currX[2] * mB[2] + 13;
+    cout << "f(x_k) = " << f << endl;
+}
+
+//вычиялем q_n=A*x_n+b
+void calculateQ(double mA[n][n], double currX[n], double mB[n]) {
+
     double summ = 0;
     for (int i = 0; i < n; i++) {
         summ = 0;
         for (int j = 0; j < n; j++) {
-            summ += mA[i][j] * currX[j];//а11*b1+a12*b2+a13*b3 
-        }                               //a21*b1+a22*b2+a23*b3 etc
-        mQ[i] = summ + mB[i];
+            summ += mA[i][j] * currX[j];
+        }
+       
+        mQ[i] = summ+mB[i];
         cout << "q[" << i << "] = " << mQ[i] << endl;
     }
-    //умножаем орт e^T на вычисленное q (получаем число)
+}
+//вычисляем m_n = - q_n^T*q_n/(q_n^T*Aq_n)
+void calculateM(double mA[n][n], double mQ[n]) {
+    //вычисляем числитель: q_n^T*q_n - получается число
     double numerator = 0;
     for (int i = 0; i < n; i++) {
-        numerator += mQ[i] * mE[i];
+          numerator += mQ[i] * mQ[i];
     }
-    cout << "Result e^T*(Ax+b) (numerator): " << numerator << endl;
+    cout << "Result q^T*q (numerator): " << numerator << endl;
 
-    return numerator;
-}
-//вычисляем знаменатель
-double calculatedenominator(double mA[n][n],double mE[n]) {
-
-    double mM[n] = { 0, 0, 0 };//вспомогательный вектор
+    double mM[n] = { 0, 0, 0 };//вспомогательная матрица для записи произведения Aq_n
     double summ = 0;
-    //вычисляем А*е, где А - матрица, е - орт
     for (int i = 0; i < n; i++) {
         summ = 0;
         for (int j = 0; j < n; j++) {
-            summ += mA[i][j] * mE[j];
+            summ += mA[i][j] * mQ[j];
         }
         mM[i] = summ;
-        cout << "A*e[" << i << "] = " << mM[i] << endl;
+        cout << "Result A*q[" << i << "] = " << mM[i] << endl;
     }
-    //умножаем вектора e^T на вычисленное A*e (получаем число)
+    //вычисляем знаменатель q_n^T*Aq_n - получаем число
     double denominator = 0;
     for (int i = 0; i < n; i++) {
-        denominator += mM[i] * mE[i];
+        denominator += mM[i] * mQ[i];       
     }
-    cout << "Result e^T*Ae (denominator): " << denominator << endl;
+    cout << "Result q^T*Aq (denominator): " << denominator << endl;
 
-    return denominator;
+    m = - numerator / denominator;//собираем конечное значение для мью
+    cout << "Result m: " << m << endl;
 }
-//вычисляем x_n = x_n-1 + m_n*e_n, а также значение функции в этой точке
-double calculateX(double currX[n],double m, double mE[n]) {
+//вычисляем x_n = x_n-1 + m_n*e_n
+void calculateX(double currX[n], double m, double mQ[n]) {
     countX++;
     for (int i = 0; i < n; i++) {
-        calcX[i] = currX[i] + m * mE[i];
+        calcX[i] = currX[i] + m * mQ[i];
         cout << "x_" << countX << "[" << i << "] = " << calcX[i] << endl;
     }
-    double f = 2 * pow(calcX[0], 2) + 4.3 * pow(calcX[1], 2) + 5.3 * pow(calcX[2], 2) + calcX[0] * calcX[1] - calcX[1] * calcX[2] + calcX[0] * calcX[2] + calcX[0] - 2 * calcX[1] + 3 * calcX[2] + 13;
-    return f;
 }
 
 int main()
 {
     cout.precision(10);
-    double mE[n] = { 0,0,0 };
-    //первые три итерации - с разными ортами: (1,0,0); (0,1,0); (0,0,1)
-    //и запоминаем в массив calcF значения фукнции в этих точках
-    for (int i = 0; i < 3; i++) {
-        mE[i] = 1;
-        cout << endl;
-        double k = calculateNumerator(mA, currX, mB, mE);
-        double s = calculatedenominator(mA, mE);
-        m = -k / s;//собираем мью для вычисления x_n = x_n-1 + m_n*e_n
-        calcF[i] = calculateX(currX, m, mE);
-        cout << "F: " << calcF[i] << endl;
-        mE[i] = 0;
-        for (int i = 0; i < n; i++) {//перезаписываем вычисленное решение в текущий вектор решений
-            currX[i] = calcX[i];
-        }
-    }
-    //после перебора всех координатных ортов от 1 до 3 в качестве очередного направления спуска назначаем тот орт, в направлении которого происходит
-    //наибольшее убывание функции
-    double delta = abs(currF-calcF[0]);//разница между занчением функции в точке (0,0,0) и в первой найденной точке
-    int best_position = 0;
-    for (int i = 0; i < 2; i++) {
-        if (abs(calcF[i] - calcF[i + 1]) > delta) {
-            delta = abs(calcF[i] - calcF[i + 1]);
-            best_position = i + 1;
-        }
-    }
-    cout << endl << "delta: " << delta << "  " << "position: " << best_position << endl;
-    //перезаписывем наш орт на тот, в напрвлении которого происходит наибольшее убывание функции
-    for (int i = 0; i < 3; i++) {
-        mE[i] = 0;
-    }
-    mE[best_position] = 1;
-    //вычисляем минимум, используя зафиксированный орт
+    func(mA, mB, currX);
+    cout << endl; 
     bool exit = false;
     while (exit == false) {
-        cout << endl;
-        double k = calculateNumerator(mA, currX, mB, mE);
-        double s = calculatedenominator(mA, mE);
-        m = -k / s;
-        calculateX(currX, m, mE);
-        //проверяем, пока все координаты решения на будут отличать менее чем на эпселон от предыдущего решения
-        if ((abs(currX[0] - calcX[0]) <= eps) and (abs(currX[1] - calcX[1]) <= eps) and (abs(currX[2] - calcX[2]) <= eps)) {
+        calculateQ(mA, currX, mB);
+        calculateM(mA, mQ);
+        calculateX(currX, m, mQ);
+        double h = sqrt((currX[0] - calcX[0]) * (currX[0] - calcX[0]) + (currX[1] - calcX[1]) * (currX[1] - calcX[1]) +
+            (currX[2] - calcX[2]) * (currX[2] - calcX[2]));
+        cout << h << endl;
+        //sqrt(summ(||currx-calcx||))
+        if(sqrt((currX[0]-calcX[0])* (currX[0] - calcX[0]) + (currX[1] - calcX[1]) * (currX[1] - calcX[1]) + 
+             (currX[2] - calcX[2]) * (currX[2] - calcX[2])) <= eps) {
             exit = true;
         }
         for (int i = 0; i < n; i++) {//перезаписываем вычисленное решение в текущий вектор решений
             currX[i] = calcX[i];
         }
-    }
+        func(mA, mB, currX);
+        cout << endl; 
+    } 
 }
